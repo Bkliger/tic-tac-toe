ca3d96017a45996d684758001b604757d47158b2
Bkliger <bkliger@comcast.net>
2016-03-20 20:53:18 -0700

    Here are the basics

--- js/app.js
+++ js/app.js
@@ -1,6 +1,47 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
+
+//watch for the click and get the value of the cell selected
+  $("div.col-xs-4.box").on("mousedown",function(event){
+    cellValue = $(this).text();
+    var $cell=$(this);
+
+  //retrieve stored writeValue (x or o)
+    var writeValue = $("#writeValue").text();
+
+// test to see if there is a value in the cell already
+    if (cellValue === 'X'|| cellValue === 'O') {
+      alert("There is already a value in this box");
+    } else {
+  // call function to write value in cell
+      writeValueFunc(writeValue, $cell);
+    }
+  });
+
+//reset the board
+$("button").on("mousedown",function(event){
+  $("div.col-xs-4.box").text("");
+});
+
+//writes value in cell
+  function writeValueFunc(writeValue, $cell) {
+     $cell.text(writeValue);
+
+     if (writeValue === "O") {
+       writeValue = "X";
+     } else {
+       writeValue = "O";
+    }
+     $("#writeValue").text(writeValue);
+
+  }
+
+
+
+
+
+
+
+
 
 });
